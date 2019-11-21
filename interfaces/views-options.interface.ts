c0e10858eb7df1260788c263d7947fd6d2e2fcb7
Anthony Wikkerink <Anthony.Wikkerink@D2L.com>
2019-11-21 14:27:51 -0500

    Fix possible runtime failure

--- interfaces/views-options.interface.ts
+++ interfaces/views-options.interface.ts
@@ -1,7 +1,19 @@
-import * as koaViews from 'koa-views';
-
-type KoaViewsOptions = Omit<Parameters<typeof koaViews>[1], 'autoRender'>;
-
-export interface ViewsOptions extends KoaViewsOptions {
+export interface ViewsOptions {
     dir: string;
+    /*
+    * default extension for your views
+    */
+    extension?: string;
+    /*
+    * these options will get passed to the view engine
+    */
+    options?: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+    /*
+    * map a file extension to an engine
+    */
+    map?: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+    /*
+    * replace consolidate as default engine source
+    */
+    engineSource?: any; // eslint-disable-line @typescript-eslint/no-explicit-any
 }
